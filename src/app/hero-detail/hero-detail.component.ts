549f8411bca9bc9a9f99e20ced2a5d28eb71a88d
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-11-26 02:18:36 +1000

    lesson 3 - done

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CommonModule, NgIf, UpperCasePipe } from '@angular/common';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,7 +8,7 @@ import { Hero } from '../hero';
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css'],
-  imports: [FormsModule, CommonModule, NgIf, UpperCasePipe],
+  imports: [FormsModule, NgIf, UpperCasePipe],
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { NgFor } from '@angular/common';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
@@ -10,11 +9,12 @@ import { HEROES } from '../mock-heroes';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
+  imports: [HeroDetailComponent, NgFor],
 })
 export class HeroesComponent {
   heroes = HEROES;
-  selectedHero?: Hero;
+
+  selectedHero!: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
